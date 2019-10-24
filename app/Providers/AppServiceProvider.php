ee8a2b1bbca611ed28ded79b2048a51fc611c8f6
Wellington A Gomes <wellington.alves.gomes@gmail.com>
2019-10-24 13:57:43 -0300

    Fix error mariaDB.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use App\Observers\UserObserver;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         User::observe(UserObserver::class);
+        Schema::defaultStringLength(191);
     }
 
     /**
